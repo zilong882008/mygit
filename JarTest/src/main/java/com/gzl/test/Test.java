17c103040a001cb7ef6906136cb250140c603c95
zilong882008 <zilong882008@163.com>
2018-10-31 22:40:52 +0800

    fdsa

--- JarTest/src/main/java/com/gzl/test/Test.java
+++ JarTest/src/main/java/com/gzl/test/Test.java
@@ -2,8 +2,21 @@ package com.gzl.test;
 
 public class Test {
 	public static void main(String[] args) {
-		for(int i=0;i<10;i++){
-			System.out.println("a:"+i);
-		}
+		 Integer i1=new Integer(100);
+		 Integer i2=new Integer(100);
+		 int i3=100;
+		 Integer i4=100;
+		 Integer i5=100;
+		 
+		 Integer i6=100;
+		 Integer i7=100;
+		 System.out.println(i1==i2);
+		 System.out.println(i1.equals(i2));
+		 System.out.println(i1==i3);
+		 System.out.println(i1.equals(i3));
+		 System.out.println(i1==i4);
+		 System.out.println(i4==i5);
+		 System.out.println(i6==i7);
+		  
 	}
 }
